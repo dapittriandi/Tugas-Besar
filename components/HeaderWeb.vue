bf5cfdd9432f9582b216f28157d81e45d907b90b
dapittriandi <dpttrndi88@gmail.com>
2023-08-28 23:33:16 +0700

    Commit Tugas latihan1

---
+++ components/HeaderWeb.vue
@@ -0,0 +1,60 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+    <h1><a class="navbar-brand" href="#">
+      Bumi Style
+    </a></h1>
+    </div>
+    <div id="navbarRightAlignExample" class="collapse navbar-collapse">
+      <!-- Left links -->
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">History</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li id="login" class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+    </div>
+  <!-- Container wrapper -->
+</nav>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.navbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 80px;
+    background: #e3e6f3;
+    box-shadow: 0 5px 15px rgba(0,0,0,0.06);
+    z-index: 999;
+    position: sticky;
+    opacity: 0.9;
+    top: 0;
+    left: 0;
+}
+
+.navbar-brand {
+  font-size: 30px;
+  font-style: italic;
+}
+
+.navbar-nav .nav-item {
+  font-size: 18px;
+  font-family: 'attack', sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="container-xxl my-container">
+        <div class="hero">
+           <h2>Sustainable <span class="text1">Clothing</span> with a <br> <span class="text2">Creative</span> Touch.</h2>
+            <a href="#" class="btn active d-grid col-6 mx-auto" style="font-weight:500;color: #efefef;width: 220px;" role="button" data-bs-toggle="button" aria-pressed="true">Shop Now</a>
+        </div>
+        <div class="card-group" >
+            <div class="card card1">
+                <img src="~/assets/cloth9.jpg" class="card-img" alt="">
+            </div>
+            <div class="card card2">
+                <img src="~/assets/cloth8.jpg" class="card-img" alt="">
+            </div>           
+        </div>
+        <div class="card-group">
+            <div class="card">
+                <img src="~/assets/cloth1.png" class="card-img" alt="">
+            </div>
+            <div class="card">
+                <img src="~/assets/cloth3.png" class="card-img" alt="">
+            </div>
+            <div class="card">
+                <img src="~/assets/cloth4.png" class="card-img" alt="">
+            </div>
+        </div>
+        <div class="card-group">
+            <div class="card">
+                <img src="~/assets/cloth2.png" class="card-img" alt="">
+            </div>
+            <div class="card">
+                <img src="~/assets/cloth7.png" class="card-img" alt="">
+            </div>           
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+}
+.my-container .hero{
+    height: 100vh;
+    background-image: url('~/assets/img/hero.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.my-container h2 {
+    font-size: 60px;
+    font-weight: 800;
+    text-align: center;
+    padding-top: 30%;
+    color: #2b2610c5;
+}
+
+.hero .btn a {
+    color: #efefef;
+}
+
+.card-group .card {
+    margin: 5px;
+}
+
+.card-group .card img{
+ width: 100%;
+ height: 100%;
+}
+
+.card1 {
+    min-width: 800px;
+    height: 100%;
+}
+
+.card2 {
+    max-height: 500px;
+}
+
+.card2 {
+    width: 50%;
+}
+</style>
